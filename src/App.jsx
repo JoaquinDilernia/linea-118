0a1f60ab694c277b8a14eeba7b1be6b8bdc9add4
dilerr <jdilernia99@gmail.com>
2023-12-04 17:33:49 -0300

    ultimos commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import Home from './components/Home/Home'
-import Interes from './components/Interes/Interes'
-import Preguntas from './components/Preguntas/Preguntas'
-import Galeria from './components/Galeria/Galeria'
-import Historia from './components/Historia/Historia'
-import Recorridos from './components/Recorridos/Recorridos'
-import Turismo from './components/Turismo/Turismo'
-
-
-
+import { Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import Interes from "./components/Interes/Interes";
+import Preguntas from "./components/Preguntas/Preguntas";
+import Galeria from "./components/Galeria/Galeria";
+import Historia from "./components/Historia/Historia";
+import Recorridos from "./components/Recorridos/Recorridos";
+import Turismo from "./components/Turismo/Turismo";
 
 function App() {
-
   return (
     <>
-<ChakraProvider>
+      <ChakraProvider>
+        <NavBar />
 
-<NavBar/>
-
-<Routes>
-<Route path='/' element={<Home />} />
-<Route path='/interes' element={<Interes />} />
-<Route path='/preguntas-frecuentes' element={<Preguntas />} />
-<Route path='/galeria' element={<Galeria />} />
-<Route path='/historia' element={<Historia />} />
-<Route path='/turismo' element={<Turismo />} />
-<Route path='/recorridos' element={<Recorridos />} />
-
-
-</Routes>
-</ChakraProvider>
-
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/interes" element={<Interes />} />
+          <Route path="/preguntas-frecuentes" element={<Preguntas />} />
+          <Route path="/galeria" element={<Galeria />} />
+          <Route path="/historia" element={<Historia />} />
+          <Route path="/turismo" element={<Turismo />} />
+          <Route path="/recorridos" element={<Recorridos />} />
+        </Routes>
+      </ChakraProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Preguntas/Preguntas.jsx
+++ src/components/Preguntas/Preguntas.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import './Preguntas.css'
-
+import React from "react";
+import "./Preguntas.css";
 
 const Preguntas = () => {
   return (
-    <div className='preguntas_frecuentes_contenedor'>
-
-      <div className='preguntas_frecuentes_contenedor__preguntas'>
+    <div className="preguntas_frecuentes_contenedor">
+      <div className="preguntas_frecuentes_contenedor__preguntas">
         <details open>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
+          <summary>¿Donde puedo realizar un reclamo?</summary>
+          <h5>Línea de atención gratuita</h5>
+          <p>
+            Para realizar un reclamo, comunicate con nuestra línea telefónica
+            gratuita 0800-333-0300 de lunes a viernes de 8 a 20 hs.
+          </p>
         </details>
         <details>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
+          <summary>
+            ¿Puedo subir o bajar del colectivo en un lugar distinto al de la
+            parada?
+          </summary>
+          <p>
+            Sólo podés subir o bajar en un lugar distinto al de la parada entre
+            las 22 hs y las 6 de la mañana o cuando llueve. Fuera de esos casos,
+            tenés que subir o bajar en la parada.
+          </p>
         </details>
         <details>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
+          <summary>
+            ¿El conductor del colectivo puede venderme un pasaje si olvidé mi
+            tarjeta SUBE?
+          </summary>
+          <p>No.</p>
         </details>
         <details>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
+          <summary>
+            ¿Qué debo hacer si tengo una duda respecto a mi tarjeta SUBE?
+          </summary>
+          <p>
+            El teléfono de contacto del Sistema Único de Boleto Eléctronico es
+            0800 777 7823 y su página web <a href="https://argentina.gob.ar/sube"> argentina.gob.ar/sube </a>
+          </p>
         </details>
         <details>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
+          <summary>
+            ¿Dónde puedo obtener información sobre el Boleto Estudiantil?
+          </summary>
+          <p>
+            Podés informarte en los siguientes enlaces, de acuerdo a la
+            ubicación del Establecimiento Educativo al que concurrís, ya sea en
+            la <a href="https://boletoestudiantil.buenosaires.gob.ar/"> Ciudad </a>o
+            en la <a href="https://portal.gba.gob.ar/"> Provincia </a> de Buenos
+            Aires.
+          </p>
         </details>
-        <details>
-          <summary>¿Cuáles son los horarios de atención?</summary>
-          <p>Los horarios de atención son de lunes a viernes de 8 a 18 hs.</p>
-        </details>
-
-        </div>
-
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Preguntas
+export default Preguntas;
