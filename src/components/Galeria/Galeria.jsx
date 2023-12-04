03ae2e587e23cbaa2a05fa35d06771d586f42d2e
joacodiler <jdilernia99@gmail.com>
2023-12-03 23:00:37 -0300

    casi terminado

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
+import './Galeria.css'
+
+import foto1 from '../../assets/galeria/1.jpeg'
+import foto2 from '../../assets/galeria/2.jpeg'
+import foto3 from '../../assets/galeria/3.jpeg'
+import foto4 from '../../assets/galeria/4.jpeg'
+import foto5 from '../../assets/galeria/5.jpeg'
+import foto6 from '../../assets/galeria/6.jpeg'
+import foto7 from '../../assets/galeria/7.jpeg'
+import foto8 from '../../assets/galeria/8.jpeg'
+import foto9 from '../../assets/galeria/9.jpeg'
+import foto10 from '../../assets/galeria/10.jpeg'
+import foto11 from '../../assets/galeria/11.jpeg'
+import foto12 from '../../assets/galeria/12.jpeg'
+import foto13 from '../../assets/galeria/13.jpeg'
+import foto14 from '../../assets/galeria/14.jpeg'
+import foto15 from '../../assets/galeria/15.jpeg'
+import foto16 from '../../assets/galeria/16.jpeg'
+import foto17 from '../../assets/galeria/17.jpeg'
+
+
+
 
 const Galeria = () => {
   return (
-    <div>
-      galeria
+
+    <div className="galeria">
+      <h3>Galeria</h3>
+      <div className="galeria__contenedor">
+        <img src={foto1} alt="foto1" />
+        <img src={foto2} alt="foto2" />
+        <img src={foto3} alt="foto3" />
+        <img src={foto4} alt="foto4" />
+        <img src={foto5} alt="foto5" />
+        <img src={foto6} alt="foto6" />
+        <img src={foto7} alt="foto7" />
+        <img src={foto8} alt="foto8" />
+        <img src={foto9} alt="foto9" />
+        <img src={foto10} alt="foto10" />
+        <img src={foto11} alt="foto11" />
+        <img src={foto12} alt="foto12" />
+        <img src={foto13} alt="foto13" />
+        <img src={foto14} alt="foto14" />
+        <img src={foto15} alt="foto15" />
+        <img src={foto16} alt="foto16" />
+        <img src={foto17} alt="foto17" />
+      </div>
     </div>
+
   )
 }
 
--- src/components/Recorridos/Recorridos.jsx
+++ src/components/Recorridos/Recorridos.jsx
@@ -1,69 +1,132 @@
-import React from 'react'
-import './Recorridos.css'
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import "./Recorridos.css";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-
-// import required modules
-import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
-
-import foto from '../../assets/foto.jpg'
-import barriochino from '../../assets/barriochino.avif'
-import recorridos from '../../assets/recorridos.jpg'
-
-
-
+import { motion } from "framer-motion";
 
 const Recorridos = () => {
-
-
-
-
-
   return (
-    <div className="hero">
+    <div className="Recorridos">
+      <h1>Recorridos</h1>
+      <div className="Recorridos__contenedor">
+        <motion.div
+          className="Recorridos__contenedor__map"
+          whileHover={{ scale: 1.07 }}
+          whileTap={{ scale: 0.9 }}
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <iframe src="https://www.google.com/maps/d/embed?mid=1tB489Hlp13ZpmdBSyddKHO5M6ezNzgc&ehbc=2E312F&noprof=1"></iframe>
+        </motion.div>
+        <motion.div
+          className="Recorridos__contenedor__info"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h5>IDA A al PREDIO DE REGULACIÓN BARRANCAS DE BELGRANO:</h5>
+          <p>
+            Desde PROFESOR DOCTOR PEDRO CHUTRO y MONTESQUIEU por PROFESOR DOCTOR
+            PEDRO CHUTRO, ZAVALETA, SUÁREZ, MONASTERIO, USPALLATA, MANUEL
+            GARCIA, ALBERTI, CATULO CASTILLO, PICHINCHA, AVENIDA JUAN DE GARAY,
+            ALBERTI, HUMBERTO 1º, AVENIDA JUJUY, AVENIDA PUEYRREDÓN, AVENIDA
+            GENERAL LAS HERAS, PLAZA ITALIA, PACÍFICO, AVENIDA SANTA FE, AVENIDA
+            LUIS MARÍA CAMPOS, VIRREY DEL PINO, AVENIDA VIRREY VERTIZ, BARRANCAS
+            DE BELGRANO.
+          </p>
+        </motion.div>
+      </div>
 
-      <Swiper
-        slidesPerView={2}
-        centeredSlides={false}
-        slidesPerGroupSkip={2}
-        grabCursor={true}
-        keyboard={{
-          enabled: true,
-        }}
-        breakpoints={{
-          769: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-        }}
-        scrollbar={true}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
-        className="mySwiper"
-      >
-      <SwiperSlide className='SwiperSlide' > <img src={foto} alt="" /> </SwiperSlide>
-      <SwiperSlide className='SwiperSlide' > <p>recorrido b </p> </SwiperSlide>  
+      <div className="Recorridos__contenedor">
+        <motion.div
+          className="Recorridos__contenedor__map"
+          whileHover={{ scale: 1.07 }}
+          whileTap={{ scale: 0.9 }}
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <iframe src="https://www.google.com/maps/d/embed?mid=1G7paB_2IIN7QrMn6gz5OTnE5GzMoFsM&ehbc=2E312F&noprof=1"></iframe>
+        </motion.div>
+        <motion.div
+          className="Recorridos__contenedor__info"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h5> VUELTA A REGRESO A PARQUE PATRICIOS:</h5>
+          <p>
+            Desde BARRANCAS DE BELGRANO por AVENIDA VIRREY VÉRTIZ, AVENIDA
+            VIRREY VÉRTIZ, AVENIDA LUIS MARIA CAMPOS, ARTURO ANTONIO DRESCO,
+            AVENIDA SANTA FE, PACÍFICO, AVENIDA SANTA FE, PLAZA ITALIA, AVENIDA
+            GENERAL LAS HERAS, AVENIDA PUEYRREDÓN, AVENIDA JUJUY, AVENIDA
+            BRASIL, PICHINCHA, 15 DE NOVIEMBRE DE 1889, AVENIDA JUJUY, AVENIDA
+            COLONIA, AVENIDA AMANCIO ALCORTA, LAFAYETTE, SUAREZ, LAVARDEN OESTE,
+            AVENIDA AMANCIO ALCORTA, LAVARDEN, LOS PATOS, LUNA, PROFESOR DOCTOR
+            PEDRO CHUTRO hasta MONTESQUIEU.
+          </p>
+        </motion.div>
+      </div>
 
-      <SwiperSlide className='SwiperSlide' ><img src={foto} alt="" /></SwiperSlide>
-      <SwiperSlide className='SwiperSlide' >  <p>recorrido c </p> </SwiperSlide>
+      <div className="Recorridos__contenedor">
+        <motion.div
+          className="Recorridos__contenedor__map"
+          whileHover={{ scale: 1.07 }}
+          whileTap={{ scale: 0.9 }}
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <iframe src="https://www.google.com/maps/d/embed?mid=1zUBxwHIt9y2ne4QDS9y9G5wXuGY12LU&ehbc=2E312F&noprof=1"></iframe>
+        </motion.div>
+        <motion.div
+          className="Recorridos__contenedor__info"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h5>IDA A al PREDIO DE REGULACIÓN BARRANCAS DE BELGRANO: </h5>
 
-      <SwiperSlide className='SwiperSlide' ><img src={foto} alt="" /></SwiperSlide>
-      <SwiperSlide className='SwiperSlide' >  <p>recorrido d </p> </SwiperSlide>
-    </Swiper>
-  </div>
+          <p>
+            Desde ESTACION ONCE, AVENIDA PUEYRREDÓN, AVENIDA GENERAL LAS
+            HERAS, PLAZA ITALIA, CENTRO DE TRANSBORDO PACÍFICO, AVENIDA SANTA
+            FE, AVENIDA LUIS MARÍA CAMPOS, VIRREY DEL PINO, AVENIDA VIRREY
+            VERTIZ, BARRANCAS DE BELGRANO.
+          </p>
+        </motion.div>
+      </div>
 
+      <div className="Recorridos__contenedor">
+        <motion.div
+          className="Recorridos__contenedor__map"
+          whileHover={{ scale: 1.07 }}
+          whileTap={{ scale: 0.9 }}
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <iframe src="https://www.google.com/maps/d/embed?mid=1sI0eaCBu-I9vt26Rz7hH17TBF1M8R8Y&ehbc=2E312F"></iframe>
+        </motion.div>
+        <motion.div
+          className="Recorridos__contenedor__info"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h5>REGRESO A ESTCACION ONCE</h5>
+          <p>
+            Desde BARRANCAS DE BELGRANO por AVENIDA VIRREY VÉRTIZ, ingreso a
+            carriles exclusivos CENTRO DE TRANSBORDO BARRANCAS DE BELGRANO, giro
+            en U a la altura de AVENIDA JURAMENTO, AVENIDA VIRREY VÉRTIZ,
+            AVENIDA LUIS MARIA CAMPOS, ARTURO ANTONIO DRESCO, AVENIDA SANTA FE,
+            CENTRO DE TRANSBORDO PACÍFICO, salida de carriles exclusivos CENTRO,
+            AVENIDA SANTA FE, PLAZA ITALIA, AVENIDA GENERAL LAS HERAS, AVENIDA
+            PUEYRREDÓN hasta ESTACION ONCE.
+          </p>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Recorridos
+export default Recorridos;
